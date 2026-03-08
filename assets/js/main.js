b982892cb37dfcb4e83d91621d7b71967f45bc11
Tiago Oliveira <oliveira.taquari@gmail.com>
2026-03-08 15:40:17 -0300

    aer

--- assets/js/main.js
+++ assets/js/main.js
@@ -187,6 +187,16 @@
   });
 
   /**
+   * Frequently Asked Questions Toggle (Add Tiago)
+   */
+  document.querySelectorAll('.faq-item h3, .faq-item .faq-toggle, .faq-item .faq-header').forEach((faqItem) => {
+    faqItem.addEventListener('click', () => {
+      faqItem.parentNode.classList.toggle('faq-active');
+    });
+  });
+  
+
+  /**
    * Navmenu Scrollspy
    */
   let navmenulinks = document.querySelectorAll('.navmenu a');
